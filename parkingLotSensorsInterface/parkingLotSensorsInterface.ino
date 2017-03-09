cd9b89b7b4db2b6129600309a6a0900f24100469
bruck <bruckwendu80@gmail.com>
2017-03-09 16:45:29 +0000

    resolved merge

---
+++ parkingLotSensorsInterface/parkingLotSensorsInterface.ino
@@ -0,0 +1,63 @@
+/*
+  This script is written as a part of Smart Parking system built by Bruck Wubete, Sagar Patel and Akhil Marpadga for SYSC 3010
+  It provides an interface to parking lot Infrared sensors placed on a demo board please see the Github repo for more information
+  about the board.
+*/
+
+/*
+ * @author Bruck Wubete
+*/
+
+#define LENGTH(s) (sizeof(s)/sizeof(*s))  
+int lotPins[8] = {0,1,2,3,4,5,6,7};
+int lotValues[8] = {0,0,0,0,0,0,0,0};
+int previousLotValues[8] = {0,0,0,0,0,0,0,0};
+int difference = 200;
+//We need not continiously update the sensor value as it changes very fast
+//for now we'll update sensor values every second
+unsigned long sensorDelay = 1000;
+//last time sensors were updated
+unsigned long lastUpdateTime;
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  for(int index=0; index<LENGTH(lotPins); index++){
+    pinMode(lotPins[index], OUTPUT);
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pins sequentially:
+  for(int index=0; index<LENGTH(lotValues); index++){
+    lotValues[index] = analogRead(lotPins[index]);
+  }
+  for(int index=0; index<LENGTH(lotValues); index++){
+    if(abs(lotValues[index]-previousLotValues[index])> difference){
+      
+      //Send Signal to RPI but for now blink a light
+      digitalWrite(lotPins[index], HIGH);
+      Serial.print(index);
+      Serial.print(' ');
+      Serial.print(lotValues[index]);
+      Serial.print(' ');
+      Serial.println(previousLotValues[index]);
+      updateSensorValues();
+    }else{
+      //Nothing to do here
+      digitalWrite(lotPins[index], LOW);
+    }
+  }
+  
+  //assign previous lot values the current lot values after sensorDelay has expired
+  if(millis() - lastUpdateTime > sensorDelay){
+    updateSensorValues();
+    lastUpdateTime = millis();
+  }
+}
+
+void updateSensorValues(){
+  for(int index=0; index<LENGTH(previousLotValues); index++){
+      previousLotValues[index] = lotValues[index];
+    }
+}
